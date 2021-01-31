8b8664687d77e12a8eecd6f43dc8faba36dfe35d
Barnabas Domma <41872411+Barneyblanco@users.noreply.github.com>
2021-01-31 22:38:41 +0100

    Update gridappsd-deployment.sh

--- gridappsd-deployment.sh
+++ gridappsd-deployment.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-Green = '\033[0;32m'
-
-Yellow='\033[0;33m' 
-
-Blue='\033[0;34m'
-
-echo " ${Green} installing git"
+echo "installing git"
 
 sudo apt install git
 
 sudo apt install python3.8
 
-echo "${Green}installing python 3.8"
+echo "installing python 3.8"
 
 sudo apt install python3.8
 
@@ -32,62 +26,52 @@ bash Miniconda3-latest-Linux-x86_64.sh
 
 cd  ../
 
-echo "${Green}installing pip"
+echo "installing pip"
 
 sudo apt install python3-pip
 
-echo "${Green}installing jupyterlab"
+echo "installing jupyterlab"
 
 pip install jupyterlab
 
-echo "${Green}cloning gridappsd-python folder"
+echo "cloning gridappsd-python folder"
 
 git clone https://github.com/GRIDAPPSD/gridappsd-python.git
  
-cd /GRIDAPPSD/gridappsd-python.git
+cd gridappsd-python
 
-echo "${Green}installing python dependencies" 
+echo "installing python dependencies" 
 
 pip install -r requirements.txt
 
 pip install .
 
-echo "${Green}cloning gridappsd-docker from github"
+cd ../
+
+echo "cloning gridappsd-hackathon from github"
+
+git clone https://github.com/GRIDAPPSD/gridappsd-hackathon
+
+cd ../
+
+echo "cloning gridappsd-docker from github"
 
 git clone https://github.com/GRIDAPPSD/gridappsd-docker
 
 cd gridappsd-docker
 
-echo "${Green}installing docker"
+echo "installing docker"
 
 ./docker_install_ubuntu.sh
 
-cd ../ 
-
-echo "${Green}cloning gridappsd-hackathon from github"
-
-git clone https://github.com/GRIDAPPSD/gridappsd-hackathon
-
-cd gridappsd-docker
-
-echo "${Green}pulling and starting all microservices (docker images)"
+echo "pulling and starting all microservices (docker images)"
 
 ./run.sh -t releases_2020.09.0
 
-echo "${Yellow}restarting docker service"
-
-service docker restart
-
-cd ../
-
-echo "${Green}Running final installation and starting the application"
+echo "Running final installation and starting the application"
 
 ./run-gridappsd.sh
 
-echo "${Yellow}Re-running the app"
-
-./run-gridappsd.sh
-
-echo "${Blue}APPLICATION STARTED ON PORT 8080"
+echo "APPLICATION STARTED ON PORT 8080"
 
 
